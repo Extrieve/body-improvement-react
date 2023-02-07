672388f740dcba8628985166ac2191b407b7412e
Nick <nicolas.cinera@gmail.com>
2023-02-07 09:18:31 -0500

    Creating Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<h1>Page Not Found :(</h1>} />
       </Routes>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,9 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <p>
+        This is the home page. It is currently under construction.
+      </p>
     </div>
   )
 }
