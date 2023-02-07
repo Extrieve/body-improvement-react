1ec6fe9cadbc6544ac255fc77ea84c214b48bac1
Nick <nicolas.cinera@gmail.com>
2023-02-07 08:53:39 -0500

    Small Correction

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ const Nav = () => {
     <nav>
       <ul className={'navlist'}>
       <h1>Workout Tracker</h1>
-        <li className={'navlist'}><a href="/">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/users">Users</a></li>
         <li><a href="/workouts">Workouts</a></li>
