e1d8eaac02146479cf63f3877147704d8643f4bf
Nick <nicolas.cinera@gmail.com>
2023-03-02 21:34:06 -0500

    Class refactor :-1:

---
+++ src/pages/HomeClass.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface HomeProps {}
+interface HomeState{}
+
+class HomeClass extends React.Component<HomeProps, HomeState> {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <div className="home-box">
+                    <h2>
+                    Find your perfect workout routine by exploring our user generated content.
+                    </h2>
+                    <a href="/users">
+                    <button className="btn explore">Explore</button>
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HomeClass;
